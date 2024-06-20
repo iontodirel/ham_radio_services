22e960e4018d551a3ddabef3ca2d0564d16d78cf
iontodirel <ion.todirel@gmail.com>
2024-06-19 22:30:18 -0700

    initial set of changes for direwolf

---
+++ direwolf/entrypoint.ps1
@@ -0,0 +1,23 @@
+. ./common.ps1
+
+Invoke-Init-ExitOnError -service_name $env:SERVICE_NAME -web_service_name $env:SVC_CONTROL_WS_SERVICE_NAME -web_service_port $env:SVC_CONTROL_WS_REST_PORT
+
+Invoke-InitHardware-ExitOnError -find_devices_internal_config_file_name $find_devices_internal_config_file_name `
+                                -find_devices_file_name $find_devices_file_name `
+                                -find_devices_internal_output_json_file_name $find_devices_internal_output_json_file_name `
+                                -soundcard_test_wav_file_name $soundcard_test_wav_file_name `
+                                -service_name $service_name `
+                                -log_file_name $log_file_name `
+                                -test_audio_device $soundcard_test_sound_capture
+
+Start-Direwolf-ExitOnError -callsign $callsign `
+                           -audio_device $audio_device `
+                           -serial_port $serial_port `
+                           -modem_internal_agwp_port_number $modem_internal_agwp_port_number `
+                           -modem_internal_kiss_port_number $modem_internal_kiss_port_number `
+                           -direwolf_internal_config_file_name $direwolf_internal_config_file_name `
+                           -direwolf_internal_config_working_file_name $direwolf_internal_config_working_file_name `
+                           -modem_internal_log_directory $modem_internal_log_directory `
+                           -find_devices_output_json_file $find_devices_output_json_file `
+                           -log_file_name $log_file_name `
+                           -service_name $service_name
---
+++ direwolf/healthcheck.ps1
@@ -0,0 +1,14 @@
+. ./common.ps1
+
+Invoke-Init-ExitOnError -wait_service_enabled $False -service_name $env:SERVICE_NAME -web_service_name $env:SVC_CONTROL_WS_SERVICE_NAME -web_service_port $env:SVC_CONTROL_WS_REST_PORT
+
+Invoke-ExitIfServiceDisabled-ExitOnError -service_name $service_name -process_name "direwolf" 
+
+Get-SerialPort-ExitOnError -find_devices_config_file $find_devices_internal_config_file_name -find_devices_utility $find_devices_file_name
+Get-AudioDevice-ExitOnError -find_devices_config_file $find_devices_internal_config_file_name -find_devices_utility $find_devices_file_name
+
+Test-AudioDeviceVolume-ExitOnError -find_devices_config_file $find_devices_internal_config_file_name -find_devices_utility $find_devices_file_name
+
+Test-TcpConnection-ExitOnError -host_name $service_name -port_number $modem_internal_kiss_port_number
+
+exit 0
